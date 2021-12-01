4960fa81b5e75621d6767230347b55bcec18e77b
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-11-30 22:02:26 -0600

    Internet is finally back on woohoo

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,39 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./posts-model')
+
+const router = express.Router()
+
+// Get posts
+
+router.get('/', (req,res) => {
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: 'The posts information could not be retrieved'
+            })
+        })
+})
+
+// Get post by id 
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist "})
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: "The post information could not be retrieved"})
+        })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
+const express = require('express');
+const portsRouter = require('./posts/posts-router')
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/posts', portsRouter)
+
 // require your posts router and connect it here
+
+
+module.exports = server;
