532c6fc239b598cfdf0f2a0697b68e7347fec420
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-01 15:07:36 -0600

    Finished

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -111,19 +111,23 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-router.get('/:id/comments', (req, res) => {
-    const { id } = req.params
-    Posts.findCommentById(id)
-        .then(post => {
-            if(!post) {
-                res.status(404).json({ message: "The post with the specified ID does not exist"})
-            } else {
-                console.log(post)
-            }
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id)
+        if (!post) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const comments = await Posts.findPostComments(req.params.id)
+            res.json(comments)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved",
+            error: err.message
         })
-        .catch (err => {
-            res.status(500).json({ message: "The comments information could not be retrieved", error: err.message})
-        })
+    }
 })
 
 module.exports = router;
