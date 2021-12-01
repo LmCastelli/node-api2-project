2736e5cb1e8aa8b3460d47f2303cb153240fb96d
Lucas Castelli <Lucas@Lucass-MacBook-Pro-2.local>
2021-12-01 10:11:33 -0600

    Worked on it some more now that I have internet again

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -42,8 +42,9 @@ router.post('/', async (req, res) => {
             res.status(400).json({ message: "Please provide title and contents for the post"})
         } else {
             const newPost =  await Posts.insert(req.body)
-
-            res.status(201).json(newPost)
+            res.status(201).json
+            res.json(newPost)
+            
             
         }
     } catch (err) {
@@ -54,6 +55,55 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.put('/')
+router.put('/:id', async (req, res) => {
+    const { id } = req.params
+    const { body } = req
+    try {
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({ message: "Please provide title and contents for the post"})
+        } else {
+            const updated = await Posts.update(id, body)
+            if (!updated) {
+                res.status(404).json({ message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json
+                res.json(updated)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Posts.remove(id)
+        .then(deletedPost => {
+            if (!deletedPost) {
+                res.status(404).json({ message: "The post with the specified ID does not exist"})
+            } else {
+                res.status(200).json
+                res.json(deletedPost)
+            }
+        }) 
+        .catch(err => {
+            res.status(500).json({ message: "The post with the specified ID does not exist", error: err.message})
+        })
+})
+
+router.get('/:id/comments', (req, res) => {
+    const { id } = req.params
+    Posts.findCommentById(id)
+        .then(post => {
+            if(!post) {
+                res.status(404).json({ message: "The post with the specified ID does not exist"})
+            } else {
+                console.log(post)
+            }
+        })
+        .catch (err => {
+            res.status(500).json({ message: "The comments information could not be retrieved"})
+        })
+})
 
 module.exports = router;
